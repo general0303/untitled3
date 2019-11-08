c70a56c47ed3da059f2d3cbdfbeb7007b5499c24
general0303 <psh030302@gmail.com>
2019-11-08 11:20:44 +0300

    jhds

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
         System.out.print("123345");
         System.out.print("jyfujtdutmdh");
         int i=0;
+        int g=0;
         int[] arr = new int[10];
         for(int curr: arr)
             System.out.print(curr + " ");
