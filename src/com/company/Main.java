e14446dc36b5761b859510a42dd035bb9aa3cdfd
dopaciones <jollypuff9@gmail.com>
2019-11-08 11:21:46 +0300

    array size changed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,9 @@ package com.company;
 public class Main {
     public static void Main(String[] args){
         System.out.print("123345");
-        int[] arr = new int[10];
+        int[] arr = new int[]{5,3,2};
         for(int curr: arr)
             System.out.print(curr + " ");
+
     }
 }
