153b784917d7695c897daed3d7d3de492f0e7938
general0303 <psh030302@gmail.com>
2019-11-08 11:26:03 +0300

    hlhfgjfkgl

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.print("jyfujtdutmdh");
         int i=0;
         int g=0;
+        int fuhg=9876;
         int[] arr = new int[10];
         for(int curr: arr)
             System.out.print(curr + " ");
