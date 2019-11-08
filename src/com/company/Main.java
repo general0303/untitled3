c57545529878555cb141e28a3f9e40e300bfa0bf
dopaciones <jollypuff9@gmail.com>
2019-11-08 11:03:07 +0300

    all is good now

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,5 +2,7 @@ package com.company;
 public class Main {
     public static void Main(String[] args){
         System.out.print("123345");
+        System.out.print("Petya, how are you?");
+        int white_noise = 0;
     }
 }
